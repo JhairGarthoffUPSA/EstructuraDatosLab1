056cc0471971f7ccbff32668222f7af46766037c
JhairGarthoffUPSA <99370193+JhairGarthoffUPSA@users.noreply.github.com>
2022-02-16 20:10:01 -0400

    Add files via upload
    Ejercicio del Grupo 6.

---
+++ Grupo 6 If-Else Jhair.py
@@ -0,0 +1,12 @@
+numero = int(input("Ingresa un número: "))
+if numero % 2 == 0:
+    print("El número es par.")
+else:
+    print("El número es impar.")
+
+#Grupo
+####################
+#Jhair Garthoff
+#Andrés Nina
+#Fabricio Peña
+#Augusto Carrizales
