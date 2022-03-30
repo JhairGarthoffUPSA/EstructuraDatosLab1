24a68fa4eac0e567cf29f11250b520bee0ff5912
JhairGarthoffUPSA <99370193+JhairGarthoffUPSA@users.noreply.github.com>
2022-03-30 19:54:55 -0400

    Add files via upload

---
+++ Ejemplo Cola.py
@@ -0,0 +1,10 @@
+#Ejemplo de cola - FIFO
+cola=[]
+#Incluir elementos - Push (apilar)
+cola.append('c')
+cola.append('a')
+cola.append('b')
+print(cola)
+#Sacar elementos - Pop! (desapilar)
+x=cola.pop(0)
+print(cola)
---
+++ Ejemplo Pila.py
@@ -0,0 +1,9 @@
+#Ejemplo de pila - LIFO (Last In First Out)
+pila = []
+#Incluir elementos - Push! (apilar)
+pila.append('c')
+pila.append('a')
+print(pila)
+#Sacar elementos - Pop! (desapilar)
+x=pila.pop()
+print(pila)    #lo restante
